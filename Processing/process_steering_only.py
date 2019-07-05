9ecc7d60d5dd5e262d215faead365987d86f8515
callummole <c.d.mole@leeds.ac.uk>
2019-07-05 17:54:44 +0100

    add script to collate only steering files

---
+++ Processing/process_steering_only.py
@@ -0,0 +1,32 @@
+###script to view and almagamate eye tracking files.
+
+import os
+import pandas as pd
+from glob import glob
+
+
+if __name__ == '__main__':
+	#rootdir = sys.argv[1] 
+    
+    rootdir = "C:\\VENLAB data\\SP_18-19\\Data\\pilot_orca19"
+    
+    output_filename = 'collated_steering.csv'
+
+    master_stitch = pd.DataFrame() #master data for gaze and steering          
+
+    for fn in glob(rootdir + "/*.csv"):
+        
+        print(fn)
+        
+
+        trial_data = pd.read_csv(fn)
+        #print(trial_data)
+
+            
+        master_stitch = pd.concat([master_stitch,trial_data])
+        
+    #now you've built the master data of all trials, save it.
+    master_stitch.to_csv(rootdir + "/" + output_filename)
+
+
+
